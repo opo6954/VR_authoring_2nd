206ac52a3c2b042a85e9d7dba6294ec9cf325d86
pcw1317 <pcw1317@gmail.com>
2017-01-09 20:49:24 +0900

    Added SimpleInteractionFireAlarm
    SimpleInteractionClientState에서 지정하는 상호작용가능한 오브젝트로서 화재경보기를 구현하였다.
    on/off 상태에 따라 화재경보음이 들리거나 꺼진다.

---
+++ ocean_project_2nd/VR_Authoring/Assets/Script/ClientState/SimpleInteractionFireAlarm.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleInteractionFireAlarm : SimpleInteractionModuleTemplate {
+	public override bool isEnded () {
+		return false;
+	}
+
+	public override void stateOn() {
+		if (curState)
+			return;
+		gameObject.GetComponent<AudioSource> ().mute = false;
+		curState = true;
+	}
+	public override void stateOff() {
+		if (!curState)
+			return;
+		gameObject.GetComponent<AudioSource> ().mute = true;
+		curState = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
