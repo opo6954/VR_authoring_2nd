7415be84cf6eef439319924d262d5227b213e3f4
opo6954 <leehyeopwoo@kaist.ac.kr>
2017-01-10 20:40:29 +0900

    asdf
    adsf

--- ocean_project_2nd/VR_Authoring/Assets/Demo_tmp.cs
+++ ocean_project_2nd/VR_Authoring/Assets/Demo_tmp.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class Demo_tmp : MonoBehaviour {
-    bool isTeleport = false;
+    bool isFirstTeleport = false;
+    bool isSecondTeleport = false;
     public Transform myTransform;
 	// Use this for initialization
 	void Start () {
@@ -11,12 +12,26 @@ public class Demo_tmp : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (isTeleport == false)
+        if (isSecondTeleport == false)
         {
+            if(Input.GetKeyDown("r") == true)
+            {
             myTransform.transform.position = new Vector3(126.79f, -40.123f, -0.175f);
             myTransform.transform.rotation = Quaternion.Euler(new Vector3(0.0f, -269.031f, 0.0f));
 
-            isTeleport = true;
+            isSecondTeleport = true;
+        }
+            if (isFirstTeleport == false)
+            {
+                if (Input.GetKeyDown("t") == true)
+                {
+                    myTransform.transform.position = new Vector3(-22.15f, 0.86f, 5.3f);
+                    myTransform.transform.rotation = Quaternion.Euler(new Vector3(0.0f,-268.4f,0.0f));
+
+                    isFirstTeleport = true;
+
+                }
+            }
         }
 	}
 }
